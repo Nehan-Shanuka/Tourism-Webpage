3e97bf5fc7e32d753794e4804479aedc0071979f
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-04-04 11:52:03 +0530

    Modified one

---
+++ commentformjs.js
@@ -0,0 +1,10 @@
+function showPopup() {
+    document.getElementById("popup").id = "open-popup";
+    document.getElementById("container").id = "open-container";
+    form.action;
+
+}
+function hidePopup() {
+    document.getElementById("open-popup").id = "popup";
+    document.getElementById("open-container").id = "container";
+}
